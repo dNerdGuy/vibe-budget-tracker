34310cf4332a36328ac7a213cece9a7060c17204
ghimirekto <atishghimire21000@gmail.com>
2025-06-26 14:28:00 +0545

    Add domain in cookie options for safari

--- apps/backend/src/config/cookies.ts
+++ apps/backend/src/config/cookies.ts
@@ -1,12 +1,18 @@
+import { CookieOptions } from "hono/utils/cookie";
 import { env, isProduction } from "../config/env";
 
 // Cookie configuration helper for consistent cookie settings across the app
-export const cookieConfig = {
+const domain = new URL(env.CORS_ORIGIN).hostname;
+export const cookieConfig: Record<
+  "accessToken" | "refreshToken",
+  CookieOptions
+> = {
   accessToken: {
     httpOnly: true,
     secure: env.COOKIE_SECURE ?? isProduction,
     sameSite: env.COOKIE_SAME_SITE,
     maxAge: env.COOKIE_ACCESS_TOKEN_MAX_AGE,
+    domain,
     path: "/",
   },
   refreshToken: {
@@ -14,6 +20,7 @@ export const cookieConfig = {
     secure: env.COOKIE_SECURE ?? isProduction,
     sameSite: env.COOKIE_SAME_SITE,
     maxAge: env.COOKIE_REFRESH_TOKEN_MAX_AGE,
+    domain,
     path: "/",
   },
 } as const;
